192686a4f3fd1e320c9c1d5bb943bbefc554540e
goodudetheboy <hochivuong2002@gmail.com>
2021-11-29 04:57:05 -0500

    Format detailed string of tickets

--- app/src/main/java/com/zendesk/zccvho/ticketviewer/Ticket.java
+++ app/src/main/java/com/zendesk/zccvho/ticketviewer/Ticket.java
@@ -2,6 +2,8 @@ package com.zendesk.zccvho.ticketviewer;
 
 import java.util.List;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 public class Ticket {
     String          assigneId;
     boolean         canBeSolvedByMe;
@@ -32,14 +34,28 @@ public class Ticket {
         // empty constructor
     }
 
+    public String toDetailedString() {
+        StringBuilder sb = new StringBuilder();
+        String fDesc = WordUtils.wrap(description, 60, "\n", true);
+
+        sb.append("\t From: " + requesterId + "\n");
+        sb.append("\t Subject: " + subject + "\n\n");
+        sb.append("\t\t " + fDesc + "\n\n");
+        sb.append("\t Created at: " + createdAt + "\t\t Status: " + status + "\n");
+
+        return sb.toString();
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+
         sb.append("[" + status + "]  \t");
         sb.append("Ticket [" + id + "] \t");
         sb.append("Subject: " + Utils.prettifyString(subject, 40) + " ");
         sb.append("- Created at: " + createdAt + " ");
         sb.append(" by: " + requesterId);
+
         return sb.toString();
     }
 }
